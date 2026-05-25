e757319f2985aae06abfbf874566eaede2210ab2
marco0520 <marco05dimic@icloud.com>
2026-05-25 12:09:36 +0200

    Update hotel.h

--- Project6/hotel.h
+++ Project6/hotel.h
@@ -1,44 +1,32 @@
 #ifndef HOTEL_H
 #define HOTEL_H
 
-#define MAX_NAZIV_TIPA 50
-#define MAX_OPIS_TIPA 100
+#include "soba.h"
+
 #define MAX_BROJ_SOBA 50
-
-typedef enum {
-	SLOBODNA = 1,
-	ZAUZETA,
-	CISCENJE,
-	ODRZAVANJE
-} StatusSobe;
+#define DATOTEKA_SOBA "sobe.txt"
 
 typedef struct {
-	int id;
-	char naziv[MAX_NAZIV_TIPA];
-	char opis[MAX_OPIS_TIPA];
-	double cijenaPoNoci;
-} TipSobe;
-
-typedef struct {
-	int brojSobe;
-	int kat;
-	int kapacitet;
-	StatusSobe status;
-	TipSobe tip;
-} Soba;
-
-typedef struct {
-	Soba sobe[MAX_BROJ_SOBA];
-	int brojSoba;
+    Soba sobe[MAX_BROJ_SOBA];
+    int brojSoba;
 } Hotel;
 
 void inicijalizirajHotel(Hotel* hotel);
+
 void prikaziNaslov(void);
 void glavniIzbornik(Hotel* hotel);
 void upravljanjeSobama(Hotel* hotel);
 void upravljanjeTipovima(void);
+
 void dodajSobu(Hotel* hotel);
 void prikaziSobe(const Hotel* hotel);
-const char* statusUTekst(StatusSobe status);
+void urediSobu(Hotel* hotel);
+void obrisiSobu(Hotel* hotel);
+
+void sortirajSobePoBroju(Hotel* hotel);
+void pretraziSobuPoBroju(Hotel* hotel);
+
+void spremiSobeUDatoteku(const Hotel* hotel);
+void ucitajSobeIzDatoteke(Hotel* hotel);
 
 #endif
